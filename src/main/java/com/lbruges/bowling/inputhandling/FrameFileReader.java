c2b54b470d163f617ec2f92e3f1deecd0e7353e9
Laura <laura.bruges.h@gmail.com>
2020-12-21 11:50:28 -0500

    Refactoring file reading logic

--- src/main/java/com/lbruges/bowling/inputhandling/FrameFileReader.java
+++ src/main/java/com/lbruges/bowling/inputhandling/FrameFileReader.java
@@ -2,10 +2,14 @@ package com.lbruges.bowling.inputhandling;
 
 import com.lbruges.bowling.exception.ApplicationException;
 import com.lbruges.bowling.exception.ExceptionEnum;
+import com.lbruges.bowling.inputhandling.parser.impl.StringToFrameParser;
 import com.lbruges.bowling.model.frame.IFrame;
+import org.apache.commons.lang3.StringUtils;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Stream;
@@ -17,6 +21,8 @@ import java.util.stream.Stream;
  */
 public class FrameFileReader {
 
+    private static final StringToFrameParser STRING_TO_FRAME_PARSER = new StringToFrameParser();
+
     /**
      * Reads a text file and converts the line stream to a player frames map.
      * @param inputFilename file name
@@ -26,7 +32,12 @@ public class FrameFileReader {
     public static Map<String, List<IFrame>> getPlayerFrameMap(String inputFilename) throws ApplicationException {
         Map<String, List<IFrame>> frameMap;
         try (Stream<String> lines = Files.lines(Paths.get(inputFilename))) {
-            frameMap = InputHandler.streamToFrameMap(lines);
+            Map<String, List<String>> strRollsPerPlayer = new LinkedHashMap<>();
+            lines.filter(l -> StringUtils.isNoneBlank(l))
+                    .map(l -> l.split("\t"))
+                    .forEach(sL -> addToRollsMap(strRollsPerPlayer, sL));
+
+            frameMap = STRING_TO_FRAME_PARSER.parseTo(strRollsPerPlayer);
         } catch (ApplicationException ae) {
             throw ae;
         } catch (Exception e) {
@@ -35,4 +46,16 @@ public class FrameFileReader {
 
         return frameMap;
     }
+
+    private static void addToRollsMap(Map<String, List<String>> rollsPerPlayer, String[] splitLine) {
+        String playerName = splitLine[0].trim();
+        String roll = splitLine[1].trim();
+
+        if (!rollsPerPlayer.containsKey(playerName)) {
+            rollsPerPlayer.put(playerName, new LinkedList<>());
+        }
+
+        rollsPerPlayer.get(playerName).add(roll);
+    }
+
 }
---
+++ src/main/java/com/lbruges/bowling/inputhandling/parser/impl/StringToFrameParser.java
@@ -0,0 +1,36 @@
+package com.lbruges.bowling.inputhandling.parser.impl;
+
+import com.lbruges.bowling.exception.ApplicationException;
+import com.lbruges.bowling.inputhandling.parser.IParser;
+import com.lbruges.bowling.inputhandling.parser.impl.FrameParser;
+import com.lbruges.bowling.inputhandling.parser.impl.RollParser;
+import com.lbruges.bowling.model.frame.IFrame;
+import com.lbruges.bowling.model.roll.IRoll;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Handles the conversion from the text file lines to a bowling frame list per player using roll lists as a basis.
+ *
+ * @author Laura Bruges
+ */
+public class StringToFrameParser implements IParser<Map<String, List<String>>, Map<String, List<IFrame>>> {
+
+    private static final FrameParser FRAME_PARSER = new FrameParser();
+    private static final RollParser ROLL_PARSER = new RollParser();
+
+    @Override
+    public Map<String, List<IFrame>> parseTo(Map<String, List<String>> strRollsPerPlayer) throws ApplicationException {
+        Map<String, List<IFrame>> frameMap = new LinkedHashMap<>();
+        for (Map.Entry<String, List<String>> entry : strRollsPerPlayer.entrySet()) {
+            // This was done to avoid lambda exception handling
+            List<IRoll> rolls = ROLL_PARSER.parseTo(entry.getValue());
+            List<IFrame> frames = FRAME_PARSER.parseTo(rolls);
+            frameMap.put(entry.getKey(), frames);
+        }
+
+        return frameMap;
+    }
+}
